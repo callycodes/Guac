1e02f0933b4bcfb2565bc0b684c73900c1c961b9
callycodes <61029674+callycodes@users.noreply.github.com>
2020-04-14 13:05:33 +0100

    Tutorial, Parachute, Speedups
    Tutorial has been implemented! Main menu skipped for now, straight into parachuting tortoise scene! Automated dialogue and start of the game!
    
    Guac has new parachute animation and sprites. Gradual speedups throughout game, has to be adjusted.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ width = 1440
 screen = pygame.display.set_mode([width, height])
 
 # background = pygame.image.load("assets/background/background.png")
-logo = pygame.image.load("assets/logo.png").convert_alpha()
+logo = pygame.transform.scale(pygame.image.load("assets/logo.png").convert_alpha(), (265, 150))
 fire_frames = glob.glob("assets/guaca/fire/*.png")
 
 text = pygame.font.Font("assets/fonts/pixelated.ttf", 18)
@@ -45,20 +45,24 @@ class Layer:
         self.speed = options.speed
 
 
-
-RUNNING, PAUSED, QUIT, MAIN_MENU = 0, 1, 2, 3
+RUNNING, PAUSED, QUIT, MAIN_MENU, INTRO = 0, 1, 2, 3, 4
 
 
 class GameOptions:
     def __init__(self):
         self.score = 0
-        self.speed = 3
-        self.state = RUNNING
+        self.speed = 0
+        self.state = INTRO
 
     def draw(self):
         score = menu.render("SCORE: " + str(self.score), False, (255, 255, 255))
         screen.blit(score, (player.x, player.y - 100))
         # screen.blit(score, ((width // 2 - score.get_width() // 2), 10))
+
+    def speedup(self):
+        if self.speed < 11:
+            self.speed += 1
+            player.set_velocity()
 
 
 options = GameOptions()
@@ -109,6 +113,13 @@ class Dialogue:
         print("dialogue created")
         pygame.time.set_timer(dialogue_event, 5000)
 
+    def say_line(self, line):
+        print("created")
+        self.message = line
+        self.layer = Layer(0, "assets/dialogue/sprite_0.png", (width - 400), (height - 310))
+        self.showing = True
+        pygame.time.set_timer(dialogue_event, 5000)
+
     def display(self):
         if self.showing:
             screen.blit(self.layer.image, (self.layer.x, self.layer.y))
@@ -125,7 +136,7 @@ class Dialogue:
 
 class Logo:
     def __init__(self):
-        self.layer = Layer(5, "assets/logo.png", (width - 900) / 2, 15)
+        self.layer = Layer(5, "assets/logo.png", (width - logo.get_width()) // 4, 20)
         self.showing = True
         self.moving = False
 
@@ -137,7 +148,7 @@ class Logo:
             return True
 
         if self.showing:
-            if player_x >= 50:
+            if player_x >= 200:
                 return True
 
     def move(self):
@@ -157,14 +168,22 @@ class Logo:
 class Background(pygame.sprite.Sprite):
     def __init__(self):
 
-        self.back = Layer(1, "assets/background/l1_sprite_1.png", 0, 0)
-        self.back2 = Layer(1, "assets/background/l1_sprite_1.png", width, 0)
+        self.back = Layer(0, "assets/background/l1_sprite_1.png", 0, 0)
+        self.back2 = Layer(0, "assets/background/l1_sprite_1.png", width, 0)
 
-        self.mid = Layer(2, "assets/background/l2_sprite_1.png", 0, 26)
-        self.mid2 = Layer(2, "assets/background/l2_sprite_1.png", width, 26)
+        self.mid = Layer(0, "assets/background/l1_sprite_1.png", 0, 0)
+        self.mid2 = Layer(0, "assets/background/l1_sprite_1.png", width, 0)
 
         self.front = Layer(options.speed, "assets/background/l3_sprite_2.png", 0, 0)
         self.front2 = Layer(options.speed, "assets/background/l3_sprite_2.png", width, 0)
+
+    def set_background_speeds(self):
+        self.back.speed = 1
+        self.back2.speed = 1
+        self.mid.speed = 2
+        self.mid2.speed = 2
+        self.front.speed = options.speed
+        self.front2.speed = options.speed
 
     def update(self):
         self.front.set_speed()
@@ -199,7 +218,6 @@ class Background(pygame.sprite.Sprite):
         screen.blit(self.mid2.image, (self.mid2.x, self.mid2.y))
         screen.blit(self.front.image, (self.front.x, self.front.y))
         screen.blit(self.front2.image, (self.front2.x, self.front2.y))
-
 
 
 class SpriteAnimation:
@@ -246,6 +264,34 @@ class SpriteAnimation:
         self.opacity -= 5
 
 
+class Intro():
+    def __init__(self):
+        self.progress = 0
+
+    def tutorial(self):
+        # parachute in
+        if self.progress == 0:
+            player.parachute()
+            # parachute in
+            if player.y >= 385:
+                player.state("idle")
+                self.progress = 1
+
+        elif self.progress == 1:
+            if dialogue.showing == False:
+                dialogue.say_line("Haha! You landed right in my trap!")
+
+        elif self.progress == 2:
+            if dialogue.showing == False:
+                dialogue.say_line("Prepare to get eaten! This should be fun!")
+        elif self.progress == 3:
+            background.set_background_speeds()
+            player.state("walk")
+            options.state = RUNNING
+            options.speed = 3
+            player.set_velocity()
+
+
 class Guaca(pygame.sprite.Sprite):
     def __init__(self):
         self.action = "idle"
@@ -265,11 +311,28 @@ class Guaca(pygame.sprite.Sprite):
         for frame in self.fire_frames:
             self.fire_images.append(pygame.image.load(frame).convert_alpha())
 
-
-        self.x = 0
-        self.y = 385
+        self.x = 60
+        self.y = -70
         self.fire_pos = 0;
         self.sprite_animations = []
+
+        self.parachuting_sprite = pygame.image.load("assets/guaca/parachute/sprite_0.png").convert_alpha()
+        self.parachute_sprite = pygame.image.load("assets/guaca/parachute/para_0.png").convert_alpha()
+
+    def walk(self):
+        self.state("walk")
+
+    def run(self):
+        self.state("run")
+
+    def spin(self):
+        self.state("spin")
+
+    def idle(self):
+        self.state("idle")
+
+    def parachute(self):
+        self.action = "parachute"
 
     def get_rect(self):
         return pygame.Rect(self.x, self.y, 32, 32)
@@ -288,31 +351,41 @@ class Guaca(pygame.sprite.Sprite):
 
     def update(self):
 
-        self.img = self.images[self.frame_pos]
-        if self.frame_pos == self.frame_max:
-            self.frame_pos = 0
-        else:
-            self.frame_pos += 1
+        if options.state == RUNNING:
+            self.img = self.images[self.frame_pos]
+            if self.frame_pos == self.frame_max:
+                self.frame_pos = 0
+            else:
+                self.frame_pos += 1
 
-        self.x -= options.speed
-        self.x += self.velocity
+            self.x -= options.speed
+            self.x += self.velocity
 
-        if self.x <= 0:
-            self.x = 0
+            if self.x <= 50:
+                self.x = 50
 
-        if self.action == "run":
-            self.flame()
+            if self.action == "run":
+                self.flame()
 
-        for sprite in self.sprite_animations:
-            if sprite.opacity < 0:
-                self.sprite_animations.remove(sprite)
+            for sprite in self.sprite_animations:
+                if sprite.opacity < 0:
+                    self.sprite_animations.remove(sprite)
 
-            sprite.draw(self.x, self.y - 10)
+                sprite.draw(self.x, self.y - 10)
+
+        elif options.state == INTRO:
+            if self.action == "parachute":
+                self.x += 2
+                self.y += 4
+                screen.blit(self.parachute_sprite, (self.x - 10, self.y - 10))
+                self.img = self.parachuting_sprite
 
         screen.blit(self.img, (self.x, self.y))
 
     def state(self, state):
+
         if self.action != state:
+
             self.frames = glob.glob("assets/guaca/" + state + "/*.png")
             self.frame_pos = 0
             self.frame_max = len(self.frames) - 1
@@ -324,15 +397,22 @@ class Guaca(pygame.sprite.Sprite):
             self.img = self.images[0]
 
             self.action = state
+            self.set_velocity()
 
-    def move(self, speed):
-        self.velocity = speed
+    def set_velocity(self):
+        print("Velocity changed")
+        if self.action == "idle":
+            self.velocity = 0
+        elif self.action == "walk":
+            self.velocity = options.speed
+        elif self.action == "run" or self.action == "spin":
+            self.velocity = options.speed * 2
 
 
 class Components:
-    def __init__(self, name, x, y, w = 64, h = 64):
+    def __init__(self, name, x, y, w=64, h=64):
         self.frames = glob.glob("assets/components/" + name + "/*.png")
-        self.frame_pos = random.randint(0, len(self.frames)-1)
+        self.frame_pos = random.randint(0, len(self.frames) - 1)
         self.frame_max = len(self.frames) - 1
 
         self.w = w
@@ -347,7 +427,6 @@ class Components:
         self.x = x
         self.y = y
 
-
     def draw(self):
 
         self.img = self.images[self.frame_pos]
@@ -360,9 +439,9 @@ class Components:
 
 
 class Decoration:
-    def __init__(self, name, obj_type, x, y, speed = 0, w = 32, h = 32):
+    def __init__(self, name, obj_type, x, y, speed=0, w=32, h=32):
         self.frames = glob.glob("assets/environment/" + obj_type + "/" + name + "/*.png")
-        self.frame_pos = random.randint(0, len(self.frames)-1)
+        self.frame_pos = random.randint(0, len(self.frames) - 1)
         self.frame_max = len(self.frames) - 1
 
         self.w = w
@@ -377,7 +456,6 @@ class Decoration:
         self.x = x
         self.y = y
         self.speed = speed
-
 
     def get_rect(self):
         return pygame.Rect(self.x, self.y, self.w, self.h)
@@ -429,7 +507,7 @@ class Environment:
                 speed = 0
                 w = 60
                 h = 60
-            return Decoration(name, obj_type, width + random.randint(0, 2000), y, speed, w, h)
+            return Decoration(name, obj_type, width + random.randint(0, 100), y, speed, w, h)
         elif obj_type == "consumables":
             return Decoration(name, obj_type, width + random.randint(0, 2000), 400, 0, 64, 64)
 
@@ -475,8 +553,9 @@ class Environment:
             coin.draw()
 
 
-
 player = Guaca()
+introduction = Intro()
+
 environment = Environment()
 background = Background()
 logo = Logo()
@@ -486,6 +565,7 @@ timer = Components("timer", width - 200, 20, 40, 45)
 
 while True:
 
+    screen.fill((70, 140, 170))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             options.state = QUIT
@@ -500,14 +580,11 @@ while True:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                player.state("walk")
-                player.move(3)
+                player.idle()
             if event.key == pygame.K_RIGHT:
-                player.state("run")
-                player.move(6)
+                player.run()
             if event.key == pygame.K_DOWN:
-                player.state("spin")
-                player.move(10)
+                player.spin()
             if event.key == pygame.K_UP:
                 dialogue.say()
             if event.key == pygame.K_1:
@@ -518,24 +595,38 @@ while True:
                 options.state = RUNNING
 
         if event.type == pygame.KEYUP:
-            player.move(0)
-            player.state("idle")
+            if options.state == RUNNING:
+                player.walk()
 
         if event.type == dialogue_event:
+            print("found dialogue")
             dialogue.destroy()
+            if options.state == INTRO:
+                print("is intro dialogue")
+                introduction.progress += 1
 
-        if event.type == speedup_event:
-            options.speed += 1
+        if event.type == speedup_event & options.state != INTRO:
+            options.speedup()
 
     # Fill the background with white
     # screen.blit(background, (0, 0))
-    if options.state == RUNNING:
-        background.update()
 
+    if options.state == MAIN_MENU:
         if logo.remove_check(player.x):
             logo.move()
         else:
             logo.draw()
+
+    if options.state == INTRO:
+        # print("intro")
+        background.update()
+
+        introduction.tutorial()
+        player.update()
+        dialogue.display()
+
+    elif options.state == RUNNING:
+        background.update()
 
         environment.loop()
 
@@ -544,9 +635,8 @@ while True:
         dialogue.display()
 
         options.draw()
-
-    state_button.draw()
-    timer.draw()
+        state_button.draw()
+        timer.draw()
 
     pygame.display.update()
 
